7858c018c297c22fcf5896bad49cc0336ebb05cf
Rorro <Rodrigo@example.com>
2019-02-23 17:34:03 -0300

    se borro active hasta poder mejorarlo posteriormente

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -22,7 +22,7 @@ class Menu extends Component {
             </button>
             <div className="collapse navbar-collapse" id="navbarNavDropdown">
               <ul className="navbar-nav">
-                <li className="nav-item active">
+                <li className="nav-item">
                   <a className="nav-link" href = "/" >{ m1 } <span className="sr-only">(current)</span></a>
                 </li>
                 <li className="nav-item">
