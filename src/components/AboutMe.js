bc40a23e219ac137587dd285389c9aa6aeece5f2
Rorro <Rodrigo@example.com>
2019-03-24 17:36:09 -0300

    cambio foto

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -20,9 +20,9 @@ class AboutMe extends Component {
       const { nombre, descripcion, tecnologia1, tecnologia2, tecnologia3, social1, url1, social2, url2 } = this.props;
         return(
             <div className="row justify-content-center">
-            <div className="col mt-4 text-center">  
+            <div className="col mt-4">  
             <div className="card mx-auto mb-4" style={{width: '20rem'}} >
-            <img src="https://scontent.fccp2-1.fna.fbcdn.net/v/t1.0-9/51375859_539174683155983_7731741109113585664_n.jpg?_nc_cat=106&_nc_ht=scontent.fccp2-1.fna&oh=21cd93ba31136a71cf9249cb3df6b474&oe=5CF7C712" className="card-img-top" alt="..."/>
+            <img src="https://scontent.fccp2-1.fna.fbcdn.net/v/t1.0-9/55597449_560417617698356_809731339661082624_n.jpg?_nc_cat=111&_nc_ht=scontent.fccp2-1.fna&oh=134da914d79f96948241aca00f968c7b&oe=5D0A0C5A" className="card-img-top" alt="..."/>
             <div className="card-body">
               <h5 className="card-title">{ nombre }</h5>
               <p className="card-text">{ descripcion }</p>
