13deb477bb7f178f7b464d7f0e27939aabd2c3bd
Rorro <Rodrigo@example.com>
2019-04-28 19:26:45 -0400

    cambio interfaz

---
+++ build/precache-manifest.20f9f6ea1f07af881128f67e99411662.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "606a6345e848583817a5",
+    "url": "/static/js/main.606a6345.chunk.js"
+  },
+  {
+    "revision": "54094b48b410ee26b80e",
+    "url": "/static/js/2.54094b48.chunk.js"
+  },
+  {
+    "revision": "606a6345e848583817a5",
+    "url": "/static/css/main.5ffeb44a.chunk.css"
+  },
+  {
+    "revision": "677c473c530d797d21e5e76a16ba1c2c",
+    "url": "/index.html"
+  }
+];
